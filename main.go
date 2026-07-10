9ebca76dcb88a2742e58f75c4a7e7721a9f5498f
haarshmap <harshmap0@gmail.com>
2026-07-11 00:05:41 +0530

    db and routes changes

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"math/rand"
+	"errors"
+	"math/rand/v2"
 	"net/http"
-	"time"
 
 	"github.com/glebarez/sqlite"
 	"github.com/labstack/echo"
@@ -13,90 +13,88 @@ import (
 
 // models
 type Link struct {
-	ID  string `json:"id"`
-	URL string `json:"url"`
+	gorm.Model
+	SID string `gorm:"uniqueIndex, not null" json:"sid"`
+	URL string `gorm:"not null" json:"url"`
 }
 
-var db, err = gorm.Open(sqlite.Open("data.db"), &gorm.Config{})
+var db *gorm.DB
 
+// TODO : error handling at main func and database to not be global
 func main() {
 	e := echo.New()
+	db, err := gorm.Open(sqlite.Open("data.db"), &gorm.Config{})
+	if err != nil {
+		echo.NewHTTPError(http.StatusNotFound, "database not initialised")
+	}
+	db.AutoMigrate(&Link{})
 
 	//routes
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.Use(middleware.Secure())
 
-	e.GET("/:id", RedirectHandler)
+	e.GET("/go/:id", RedirectHandler)
 	e.GET("/", IndexHandler)
-	e.POST("/submit", SubmitHandler)
+	e.POST("/go/submit", SubmitHandler)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
 // handlers
 func RedirectHandler(c echo.Context) error {
-	id := "id"
-
-	link, found := linkMap[id]
-
-	if !found {
-		return echo.NewHTTPError(http.StatusNotFound, "Link is not found")
-	}
-
-	return c.Redirect(http.StatusMovedPermanently, link.URL)
+	return nil
 }
 
 func IndexHandler(c echo.Context) error {
 	html := `
 		<h1>Submit a new website</h1>
-		<form action="/submit" method="POST">
+		<form action="/go/submit" method="POST">
 		<label for="url">Website URL:</label>
 		<input type="text" id="url" name="url">
 		<input type="submit" value="Submit">
-		</form>
-		<h2>Existing Links </h2>
-		<ul>`
-
-	for _, link := range linkMap {
-		html += `<li><a href="/` + link.ID + `">` + link.ID + `</a></li>`
-	}
-
-	html += `</ul>`
+		</form>`
 
 	return c.HTML(http.StatusOK, html)
 }
 
 func SubmitHandler(c echo.Context) error {
-	url := "url"
-	if url == "" {
-		return echo.NewHTTPError(http.StatusBadRequest, "URL is required")
+	ID := generateUniqueID(db)
+	Url := c.FormValue("url")
+	err := db.Create(&Link{
+		SID: ID,
+		URL: Url,
+	}).Error
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "The data was not pushed")
 	}
-
-	if !(len(url) >= 4 && (url[:4] == "http" || url[:5] == "https")) {
-		url = "https://" + url
-	}
-
-	id := generateRandomString(8)
-
-	linkMap[id] = Link{ID: id, URL: url}
-
-	return c.Redirect(http.StatusSeeOther, "/")
-
+	return c.HTML(http.StatusOK,
+		"<p> The shortened code is </p>",
+	)
 }
 
 // utils
 const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 func generateRandomString(length int) string {
-	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	result := make([]byte, length)
 
-	var result []byte
-
-	for i := 0; i < length; i++ {
-		index := seededRand.Intn(len(charset))
-		result = append(result, charset[index])
+	for i := range result {
+		result[i] = charset[rand.IntN(len(charset))]
 	}
 
 	return string(result)
+}
+
+func generateUniqueID(db *gorm.DB) string {
+	var link Link
+	ID := generateRandomString(8)
+	err := db.First(&link, "sid=?", ID).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return ID
+	} else {
+		return generateUniqueID(db)
+	}
+
+	return ID
 }
